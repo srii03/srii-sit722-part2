b856335bb7ee00c3588a80a05cf8374a413adb21
srii03 <162790549+srii03@users.noreply.github.com>
2024-10-02 01:11:10 +1000

    Update models.py

--- book_catalog/models.py
+++ book_catalog/models.py
@@ -1,10 +1,13 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
+# Base class for declarative class definitions
 Base = declarative_base()
 
+# Define the Book model
 class Book(Base):
     __tablename__ = "books"
+    
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
     author = Column(String, index=True)
